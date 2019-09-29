442c7ede046e8e4fad4ade2bd068cb4fb8388308
lega z50 <legavaz@gmail.com>
2019-09-29 20:29:16 +0300

    ff

--- random.py
+++ random.py
@@ -1,6 +1,9 @@
+#создание массива из случайных чисел
+#сортировка массива
+
 import random
 #print (random.random())
-a = [random.randint(0, 9) for i in range(1,11)]
+a = [random.randint(0, 100) for i in range(1,11)]
 print('mass {0}'.format(len(a)),a)
 
 def q_sort(array):
