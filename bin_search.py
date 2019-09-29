4ea6e44f67aa66d4dabfca55449f0bfd797e638d
lega z50 <legavaz@gmail.com>
2019-09-29 15:59:14 +0300

    iki

---
+++ bin_search.py
@@ -0,0 +1,28 @@
+import os
+
+os.system('CLS')
+
+
+start_vol   =   1
+end_vol     =   100
+print('Загадайте число от {0} до {1}'.format(start_vol,end_vol))
+input('Для продолжения нажмите Enter:')
+
+rez_false   =   False
+count       =   0
+while rez_false!=True:
+    count   +=   1
+    seredina =   (end_vol +   start_vol)//2
+    print('{0} число : {1}'.format(count,seredina))
+    side_vol        =     int(input('0 - угадали, 1-меньше, 3-больше: '))
+    
+    if side_vol    ==   0:
+       rez_false    =   True
+    elif side_vol   ==   1:
+        end_vol     =   seredina
+    elif side_vol   ==   3:
+        start_vol   =   seredina   
+         
+
+print('*'*20)
+print('Загаданное число: {0} отгадано с {1} попытки'.format(seredina,count))
