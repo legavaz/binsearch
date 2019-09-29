07d7d860ec04ad118a3e8ce05dd2d800137bd8d7
lega z50 <legavaz@gmail.com>
2019-09-29 20:20:11 +0300

    mad

--- random.py
+++ random.py
@@ -2,3 +2,16 @@ import random
 #print (random.random())
 a = [random.randint(0, 9) for i in range(1,11)]
 print('mass {0}'.format(len(a)),a)
+
+def q_sort(array):
+    if len(array)<2:
+        return array
+    else:
+        pivot   =   array[0]
+        less    =   [i for i in array[1:] if i<=pivot]
+        greater =   [i for i in array[1:] if i>pivot]
+        return q_sort(less) + [pivot] + q_sort(greater)
+
+a=q_sort(a)
+print('mass {0}'.format(len(a)),a)
+
