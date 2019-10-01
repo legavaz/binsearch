6ff2c6fd0a3ce37dc25426d85bf4001a4d17615a
lega z50 <legavaz@gmail.com>
2019-10-01 20:51:36 +0300

    0110+

---
+++ hash_table.py
@@ -0,0 +1,16 @@
+
+# book_keep    =   dict()
+book_keep    =   {}
+
+book_keep["apple"]   =   0.67
+book_keep["milk"]   =   1.22
+book_keep["avocado"]   =   1.49
+
+print(book_keep)
+
+for item in book_keep:
+    print(item,book_keep[item])    
+
+# book_keep[name] =   20
+# book_keep[name] =   20
+# book_keep[name] =   20
