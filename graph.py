c5d39768dc6a27e524bfbf14b569c4ecb6606a64
lega z50 <legavaz@gmail.com>
2019-10-05 19:08:21 +0300

    new file

---
+++ graph.py
@@ -0,0 +1,38 @@
+from collections import deque
+import os
+
+os.system('CLS')
+
+graph ={}
+graph["you"]    =['alice','bob','claire']
+graph["bob"]    =['anuj','peggy']
+graph["alice"]  =['peggy']
+graph["claire"] =['tom','jonny']
+graph["anuj"]   =[]
+graph["peggy"]  =[]
+graph["tom"]    =[]
+graph["jonny"]  =[]
+
+
+
+def person_is_seller(name):
+    return name[-1]=='m'
+
+
+def serch(name):
+    search_queue = deque()
+    search_queue+=graph[name]
+    serched =   []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in serched: 
+            if person_is_seller(person):
+                print(person,'is seller Mango')
+                return True
+            else:
+                search_queue +=graph[person]
+                # print(person,'NO seller Mango')
+                serched.append(person)
+    return False
+
+serch('you')
