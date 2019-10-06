0e3e7612b74499234999e647f6710250b4144a1e
lega z50 <legavaz@gmail.com>
2019-10-06 21:41:43 +0300

    реализовання таблица на массиве

--- 6.5_dejksta.py
+++ 6.5_dejksta.py
@@ -1,7 +1,27 @@
 
-import table
+from table import Table
+import os
+os.system('CLS')
 
-tab = Table(3, 5)
-tab.set_value(0, 1, 10)
-tab.set_value(1, 2, 20)
-tab.set_value(2, 3, 30)
+tab = Table(3, 3)
+
+# заполнение 1 колонки
+tab.set_value(0, 0, 'вершина 1')
+tab.set_value(1, 0, 'вершина 2')
+tab.set_value(2, 0, 'вершина 3')
+
+# заполнение 2 колонки
+tab.set_value(0, 1, 'ab')
+tab.set_value(1, 1, 'de')
+tab.set_value(2, 1, 'mk')
+
+# заполнение 3 колонки
+tab.set_value(0, 2, 10)
+tab.set_value(1, 2, 15)
+tab.set_value(2, 2, 2)
+
+
+
+tab.print_value()
+
+# input('для продолжения Enter')
--- table.py
+++ table.py
@@ -20,3 +20,29 @@ class Table(object):
  
     def n_cols(self):
         return self.cols
+    
+    def print_value(self):
+        # item_row    =   0        
+        # while item_row<self.n_rows():
+        #     item_cols   =   0
+        #     while item_cols<self.n_cols():
+        #         print(self.get_value(item_row,item_cols),end=' ')
+        #         item_cols+=1
+        #     print()
+        #     item_row+=1 
+        for i in range(self.n_rows()):
+            for j in range(self.n_cols()):
+                print(self.get_value(i, j), end=' ')
+            print()
+        
+                        
+            
+
+
+if __name__ == "__main__":
+
+    map = Table(3, 5)
+    map.set_value(0, 1, 10)
+    map.set_value(1, 2, 20)
+    map.set_value(2, 3, 30)
+    map.print_value()
