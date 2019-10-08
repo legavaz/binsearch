4a33201617ef0a2e77bee779db41e8a41fe52356
lega z50 <legavaz@gmail.com>
2019-10-08 21:49:14 +0300

    1

---
+++ 7_1_mnogestvo.py
@@ -0,0 +1,11 @@
+import os
+os.system('CLS')
+
+fruit   =   set(['avocado','tomato','banana'])
+vegetable   =   set(['beets','carrots','tomato'])
+print('fruit',fruit)
+print('vegetable',vegetable)
+print('fruit|vegetable ',fruit|vegetable)
+print('fruit&vegetable ',fruit&vegetable)
+print('fruit-vegetable ',fruit-vegetable)
+print('vegetable-fruit ',vegetable-fruit)
